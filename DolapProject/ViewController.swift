4b384968414b1c18922262abe91c460ef33608df
Şimal Çubuk <simalcubuk@simal-macbook-pro.local>
2019-04-20 23:43:58 +0300

    Initial Commit

---
+++ DolapProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DolapProject
+//
+//  Created by Şimal Çubuk on 20.04.2019.
+//  Copyright © 2019 Şimal Çubuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
